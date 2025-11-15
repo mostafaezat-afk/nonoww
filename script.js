58dfd5273f3f9dc401d0aac9a7639f38a9ea7461
mostafaezat-afk <mostafaezat@gmail.com>
2025-11-15 14:08:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-ال
+// frames.js
+// قائمة الإطارات: يمكنك إضافة أو حذف أي عدد
+// url: رابط صورة الفريم الأصلية (PNG مفرغة)
+// thumb: رابط الصورة المصغرة التي ستظهر فوق الـ Checkbox
+
+const framesList = [
+    {
+        id: 1,
+        name: "الإطار الأول",
+        url: "https://raw.githubusercontent.com/mostafaezat-afk/nonoww/main/ff22.png", // ضع رابط الفريم 1 هنا
+        thumb: "https://raw.githubusercontent.com/mostafaezat-afk/nonoww/main/ff22.png"   // ضع رابط مصغر 1 هنا
+    },
+    {
+        id: 2,
+        name: "الإطار الثاني",
+        url: "https://raw.githubusercontent.com/mostafaezat-afk/nonoww/main/ff33", // ضع رابط الفريم 2 هنا
+        thumb: "https://raw.githubusercontent.com/mostafaezat-afk/nonoww/main/33.png"
+    },
+    {
+        id: 3,
+        name: "الإطار الثالث",
+        url: "", // ضع رابط الفريم 3 هنا
+        thumb: ""
+    },
+    {
+        id: 4,
+        name: "الإطار الرابع",
+        url: "", // ضع رابط الفريم 4 هنا
+        thumb: ""
+    }
+
+];
